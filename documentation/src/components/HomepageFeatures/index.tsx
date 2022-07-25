9a5e237a7c418eb54bfa6d07f1893b4e86bdd8ec
hedaukartik <hedaukartik2009@gmail.com>
2022-07-25 12:53:15 +0530

    update features with new svg and data

--- documentation/src/components/HomepageFeatures/index.tsx
+++ documentation/src/components/HomepageFeatures/index.tsx
@@ -11,37 +11,22 @@ type FeatureItem = {
 const FeatureList: FeatureItem[] = [
   {
     title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
+    Svg: require('@site/static/img/easy_to_use.svg').default,
+    description: <>Plug & Play. No configuration required</>,
+  },
+  {
+    title: 'Light Weight',
+    Svg: require('@site/static/img/light_weight.svg').default,
+    description: <>~ 4K Gzipped (All Inclusive of Icons, Styles, & Code)</>,
   },
   {
     title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
+    Svg: require('@site/static/img/focus.svg').default,
+    description: <>focus on your business logic, leave toaster to us!</>,
   },
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
+function Feature({ title, Svg, description }: FeatureItem) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
